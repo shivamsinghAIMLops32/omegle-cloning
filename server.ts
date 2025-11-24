55e223b282548c666438a51e07b7a348873245da
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2025-11-24 16:33:15 +0530

    feat: Configure Docker Compose for local development with live reload, `Dockerfile.dev`, and local HTTPS.

--- server.ts
+++ server.ts
@@ -7,8 +7,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = parseInt(process.env.PORT || "3000", 10);
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
